b1c422f7b87cc09781d79751cf66aca21441bdb7
WapSter <wapster92@gmail.com>
2020-11-16 21:23:34 +0300

    test

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const app = express()
 
 app.use('/storage', express.static(path.join(__dirname, 'storage')))
 app.use(bodyParser.json())
-//app.use(cors())
+app.use(cors())
 
 app.use(passport.initialize())
 require('./middleware/passport')(passport)
