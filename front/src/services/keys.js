44e889773b9b48c53413c32d041196589f202223
WapSter <wapster92@gmail.com>
2020-11-16 19:59:44 +0300

    test

--- front/src/services/keys.js
+++ front/src/services/keys.js
@@ -1,5 +1,3 @@
 export default {
-   BASE_URL: process.env.NODE_ENV === 'production' ? 'http://185.229.227.69:3001' : 'http://localhost:3000'
+   BASE_URL: 'http://185.229.227.69:3001'
 }
-
-console.log(process.env.NODE_ENV)
